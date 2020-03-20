a29690fc5dab3e44d1ebc922e65bb25242a838cd
imper <me@imper.info>
2020-03-20 08:34:50 +0100

    https://github.com/allegro/allegro-api/issues/2976

--- src/Resource/Sale/OfferVariants.php
+++ src/Resource/Sale/OfferVariants.php
@@ -4,7 +4,6 @@
 namespace Imper86\PhpAllegroApi\Resource\Sale;
 
 
-use Imper86\PhpAllegroApi\Enum\ContentType;
 use Imper86\PhpAllegroApi\Resource\AbstractResource;
 use Psr\Http\Message\ResponseInterface;
 
@@ -12,20 +11,21 @@ class OfferVariants extends AbstractResource
 {
     public function put(string $setId, array $body): ResponseInterface
     {
-        return $this->apiPut("/sale/offer-variants/{$setId}", $body, ContentType::VND_BETA_V1);
+        return $this->apiPut("/sale/offer-variants/{$setId}", $body);
     }
 
     public function get(?string $setId, ?array $query = null): ResponseInterface
     {
-        return $this->apiGet(
-            sprintf('/sale/offer-variants%s', $setId ? "/{$setId}" : ''),
-            $query,
-            ContentType::VND_BETA_V1
-        );
+        return $this->apiGet(sprintf('/sale/offer-variants%s', $setId ? "/{$setId}" : ''), $query);
     }
 
     public function delete(string $setId): ResponseInterface
     {
-        return $this->apiDelete("/sale/offer-variants/{$setId}", null, ContentType::VND_BETA_V1);
+        return $this->apiDelete("/sale/offer-variants/{$setId}");
+    }
+
+    public function post(array $body): ResponseInterface
+    {
+        return $this->apiPost('/sale/offer-variants', $body);
     }
 }
