d3b1e5145a37073cd866523e8289d00a3b74f9db
imper <me@imper.info>
2020-08-03 11:26:13 +0200

    added create_shipment.php example, and modified README.md

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 
 ## Installation
 
-Just use composer:
 ```sh
 composer require imper86/php-inpost-api
+```
+
+### HTTPlug note
+This lib uses [HTTPlug](https://github.com/php-http/httplug)
+so it doesn't depend on any http client. In order to use this
+lib you must have some [PSR-18 http client](https://www.php-fig.org/psr/psr-18)
+and [PSR-17 http factories](https://www.php-fig.org/psr/psr-17).
+If you don't know which one you shoud install you can require
+these:
+
+```sh
+composer require php-http/guzzle6-adapter http-interop/http-factory-guzzle
 ```
 
 ## Usage
@@ -12,7 +23,8 @@ Using this library is very simple, fast example should be enough
 to understand how it works.
 
 ```php
-use Imper86\PhpInpostApi\InpostApi;use Imper86\PhpInpostApi\Plugin\AcceptLanguagePlugin;
+use Imper86\PhpInpostApi\InpostApi;
+use Imper86\PhpInpostApi\Plugin\AcceptLanguagePlugin;
 
 // if you want to use all resources you must ask Inpost for
 // access token via their contact form
@@ -49,6 +61,12 @@ var_dump($api->organizations()->shipments()->get('1234'));
 If you use IDE with typehinting such as PHPStorm, you'll easily 
 figure it out. If not, please 
 [take a look in Resource directory](src/Resource)
+
+## Examples
+If you still wonder how to use this lib, checkout example
+php scripts:
+
+* [Create shipment and generate label](examples/create_shipment.php)
 
 ## Contributing
 Any help will be very appreciated :)
---
+++ examples/create_shipment.php
@@ -0,0 +1,56 @@
+<?php
+
+use Imper86\PhpInpostApi\Enum\ServiceType;
+use Imper86\PhpInpostApi\InpostApi;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$token = 'your token here';
+$organizationId = 'your organization id here';
+$isSandbox = false;
+
+$api = new InpostApi($token, $isSandbox);
+
+$response = $api->organizations()->shipments()->post($organizationId, [
+    'receiver' => [
+        'name' => 'Name',
+        'company_name' => 'Company name',
+        'first_name' => 'Jan',
+        'last_name' => 'Kowalski',
+        'email' => 'test@inpost.pl',
+        'phone' => '111222333',
+    ],
+    'parcels' => [
+        ['template' => 'small'],
+    ],
+    'insurance' => [
+        'amount' => 25,
+        'currency' => 'PLN',
+    ],
+    'cod' => [
+        'amount' => 12.50,
+        'currency' => 'PLN',
+    ],
+    'custom_attributes' => [
+        'sending_method' => 'dispatch_order',
+        'target_point' => 'KRA012',
+    ],
+    'service' => ServiceType::INPOST_LOCKER_STANDARD,
+    'reference' => 'Test',
+    'external_customer_id' => '8877xxx',
+]);
+
+$shipmentData = json_decode($response->getBody()->__toString(), true);
+
+do {
+    sleep(1);
+    $response = $api->shipments()->get($shipmentData['id']);
+    $shipmentData = json_decode($response->getBody()->__toString(), true);
+} while ($shipmentData['status'] === 'confirmed');
+
+$labelResponse = $api->shipments()->label()->get($shipmentData['id'], [
+    'format' => 'Pdf',
+    'type' => 'A6',
+]);
+
+file_put_contents('/tmp/inpost_label.pdf', $labelResponse->getBody()->__toString());
